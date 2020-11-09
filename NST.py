bef69b011bfc70edf45f135a699150f5185b708b
Aman Kumar Mallik <amanmallik11091999@gmail.com>
2020-11-10 02:48:01 +0530

    Structured the contents and added the contents

--- NST.py
+++ NST.py
@@ -17,6 +17,7 @@ import torch.optim as optim
 from torchvision.utils import save_image
 import matplotlib.pyplot as plt
 
+
 #Loadung the model vgg19 that will serve as the base model
 model=models.vgg19(pretrained=True).features
 # the vgg19 model has three components :
@@ -60,29 +61,56 @@ class VGG(nn.Module):
         return features
 
 
-#defining the image transformation steps to be performed before feeding them to the model
-loader=transforms.Compose([transforms.Resize((512,512)),transforms.ToTensor()])
-#The preprocessing steps involves resizing the image and then converting it to a tensor
 
 #defing a function that will load the image and perform the required preprocessing and put it on the GPU
 def image_loader(path):
     image=Image.open(path)
+    #defining the image transformation steps to be performed before feeding them to the model
+    loader=transforms.Compose([transforms.Resize((512,512)),transforms.ToTensor()])
+    #The preprocessing steps involves resizing the image and then converting it to a tensor
+
     image=loader(image).unsqueeze(0)
     return image.to(device,torch.float)
 
 
 
 #Loading the original and the style image
-original_image=image_loader('Content image.jpg')
-style_image=image_loader('style image.jpg')
-
+original_image=image_loader('Nikola-Tesla.jpg')
+style_image=image_loader('style.png')
 
 #Creating the generated image from the original image
 generated_image=original_image.clone().requires_grad_(True)
 
+def calc_content_loss(gen_feat,orig_feat):
+    #calculating the content loss of each layer by calculating the MSE between the content and generated features and adding it to content loss
+    content_l=torch.mean((gen_feat-orig_feat)**2)#*0.5
+    return content_l
+
+def calc_style_loss(gen,style):
+    #Calculating the gram matrix for the style and the generated image
+    batch_size,channel,height,width=gen.shape
+
+    G=torch.mm(gen.view(channel,height*width),gen.view(channel,height*width).t())
+    A=torch.mm(style.view(channel,height*width),style.view(channel,height*width).t())
+        
+    #Calcultating the style loss of each layer by calculating the MSE between the gram matrix of the style image and the generated image and adding it to style loss
+    style_l=torch.mean((G-A)**2)#/(4*channel*(height*width)**2)
+    return style_l
+
+def calculate_loss(gen_features, orig_feautes, style_featues):
+    style_loss=content_loss=0
+    for gen,cont,style in zip(gen_features,orig_feautes,style_featues):
+        #extracting the dimensions from the generated image
+        content_loss+=calc_content_loss(gen,cont)
+        style_loss+=calc_style_loss(gen,style)
+    
+    #calculating the total loss of e th epoch
+    total_loss=alpha*content_loss + beta*style_loss 
+    return total_loss
+
+
 #Load the model to the GPU
 model=VGG().to(device).eval() 
-
 
 #initialize the paramerters required for fitting the model
 epoch=1000
@@ -93,8 +121,6 @@ beta=0.01
 #using adam optimizer and it will update the generated image not the model parameter 
 optimizer=optim.Adam([generated_image],lr=lr)
 
-
-
 #iterating for 1000 times
 for e in range (epoch):
     #extracting the features of generated, content and the original required for calculating the loss
@@ -102,25 +128,8 @@ for e in range (epoch):
     orig_feautes=model(original_image)
     style_featues=model(style_image)
     
-    #initilizing the content and the style loss
-    content_loss=style_loss=0
-    
     #iterating over the activation of each layer and calculate the loss and add it to the content and the style loss
-    for gen,cont,style in zip(gen_features,orig_feautes,style_featues):
-        #extracting the dimensions from the generated image
-        batch_size,channel,height,width=gen.shape
-        #calculating the content loss of each layer by calculating the MSE between the content and generated features and adding it to content loss
-        content_loss+=torch.mean((gen-cont)**2)
-        
-        #Calculating the gram matrix for the style and the generated image
-        G=torch.mm(gen.view(channel,height*width),gen.view(channel,height*width).t())
-        A=torch.mm(style.view(channel,height*width),style.view(channel,height*width).t())
-        
-        #Calcultating the style loss of each layer by calculating the MSE between the gram matrix of the style image and the generated image and adding it to style loss
-        style_loss+=torch.mean((G-A)**2)
-    
-    #calculating the total loss of e th epoch
-    total_loss=alpha*content_loss + beta*style_loss    
+    total_loss=calculate_loss(gen_features, orig_feautes, style_featues)
     #optimize the pixel values of the generated image and backpropagate the loss
     optimizer.zero_grad()
     total_loss.backward()
@@ -130,7 +139,7 @@ for e in range (epoch):
     if(e/100):
         print(total_loss)
         
-        save_image(generated_image,"gen1.png")
+        save_image(generated_image,"gen6.png")
     
         
         
